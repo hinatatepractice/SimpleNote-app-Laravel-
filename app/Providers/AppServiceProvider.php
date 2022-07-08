169820c4f32053996e23239e14372b81ad25bd01
hinatatepractice <hinata.tezuka.foreign@gmail.com>
2022-07-08 12:20:31 +0900

    BootStrapインストール・追加

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator; //Bootstrap追加
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // ページャーでBootstrapを使う
+        Paginator::useBootstrapFive();    // Bootstrap 5
     }
 }
